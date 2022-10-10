d9a2e7f6ff5f56b1126e896a1318bb4d1d0a3457
Jamshid <raofl@mail.ru>
2022-10-10 13:45:50 +0500

    fix typo

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
